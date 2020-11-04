673cb64ab5db2b2d6965caf95214b66fc8eef805
Turnerj <git@turnerj.com>
2020-11-04 19:25:10 +1030

    Added intrinsic trimming

---
+++ 09_IntrinsicTrimmingBenchmark.cs
@@ -0,0 +1,29 @@
+﻿using BenchmarkDotNet.Attributes;
+using LevenshteinBenchmarks.Implementations;
+
+namespace LevenshteinBenchmarks
+{
+	[Config(typeof(CodeSizeConfig))]
+	public class IntrinsicTrimmingBenchmark
+	{
+		[Params(10, 100, 1000, 10000)]
+		public int NumberOfChars;
+
+		public string Source;
+		public string Target;
+
+		[GlobalSetup]
+		public void Setup()
+		{
+			Source = Utilities.BuildString("aaaaaaaaaa", NumberOfChars);
+			Target = Utilities.BuildString("aaaaaaaaaa", NumberOfChars);
+		}
+
+
+		[Benchmark(Baseline = true)]
+		public int NormalTrimming() => LevenshteinPointersEverywhere.GetDistance(Source, Target);
+
+		[Benchmark]
+		public int IntrinsicTrimming() => LevenshteinIntrinsicTrimming.GetDistance(Source, Target);
+	}
+}
---
+++ Implementations/08_IntrinsicTrimming.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Buffers;
+using System.Numerics;
+using System.Runtime.Intrinsics;
+using System.Runtime.Intrinsics.X86;
+
+namespace LevenshteinBenchmarks.Implementations
+{
+	static class LevenshteinIntrinsicTrimming
+	{
+		public unsafe static int GetDistance(string source, string target)
+		{
+			var startIndex = 0;
+			var sourceEnd = source.Length;
+			var targetEnd = target.Length;
+
+			fixed (char* sourcePtr = source)
+			fixed (char* targetPtr = target)
+			{
+				var charactersAvailableToTrim = Math.Min(targetEnd, sourceEnd);
+
+				if (Avx2.IsSupported)
+				{
+					var sourceUShortPtr = (ushort*)sourcePtr;
+					var targetUShortPtr = (ushort*)targetPtr;
+
+					while (charactersAvailableToTrim >= Vector256<ushort>.Count)
+					{
+						var match = (uint)Avx2.MoveMask(
+							Avx2.CompareEqual(
+								Avx.LoadDquVector256(sourceUShortPtr + startIndex),
+								Avx.LoadDquVector256(targetUShortPtr + startIndex)
+							).AsByte()
+						);
+
+						if (match != uint.MaxValue)
+						{
+							var remaining = BitOperations.TrailingZeroCount(match ^ uint.MaxValue) / sizeof(ushort);
+							startIndex += remaining;
+							charactersAvailableToTrim -= remaining;
+							break;
+						}
+
+						startIndex += Vector256<ushort>.Count;
+						charactersAvailableToTrim -= Vector256<ushort>.Count;
+					}
+
+					while (charactersAvailableToTrim >= Vector256<ushort>.Count)
+					{
+						var match = (uint)Avx2.MoveMask(
+							Avx2.CompareEqual(
+								Avx.LoadDquVector256(sourceUShortPtr + sourceEnd - Vector256<ushort>.Count),
+								Avx.LoadDquVector256(targetUShortPtr + targetEnd - Vector256<ushort>.Count)
+							).AsByte()
+						);
+
+						if (match != uint.MaxValue)
+						{
+							var lastMatch = BitOperations.LeadingZeroCount(match ^ uint.MaxValue) / sizeof(ushort);
+							sourceEnd -= lastMatch;
+							targetEnd -= lastMatch;
+							break;
+						}
+
+						sourceEnd -= Vector256<ushort>.Count;
+						targetEnd -= Vector256<ushort>.Count;
+						charactersAvailableToTrim -= Vector256<ushort>.Count;
+					}
+				}
+
+				while (charactersAvailableToTrim > 0 && source[startIndex] == target[startIndex])
+				{
+					charactersAvailableToTrim--;
+					startIndex++;
+				}
+
+				while (charactersAvailableToTrim > 0 && source[sourceEnd - 1] == target[targetEnd - 1])
+				{
+					charactersAvailableToTrim--;
+					sourceEnd--;
+					targetEnd--;
+				}
+			}
+
+			var sourceLength = sourceEnd - startIndex;
+			var targetLength = targetEnd - startIndex;
+
+			if (sourceLength == 0)
+			{
+				return targetLength;
+			}
+
+			if (targetLength == 0)
+			{
+				return sourceLength;
+			}
+
+			var sourceSpan = source.AsSpan().Slice(startIndex, sourceLength);
+			var targetSpan = target.AsSpan().Slice(startIndex, targetLength);
+
+			var previousRow = ArrayPool<int>.Shared.Rent(targetSpan.Length);
+
+			fixed (int* previousRowPtr = previousRow)
+			fixed (char* sourcePtr = sourceSpan)
+			fixed (char* targetPtr = target)
+			{
+				for (var columnIndex = 0; columnIndex < targetLength; columnIndex++)
+				{
+					previousRowPtr[columnIndex] = columnIndex;
+				}
+
+				for (var rowIndex = 0; rowIndex < sourceLength; rowIndex++)
+				{
+					var lastSubstitutionCost = rowIndex;
+					var lastInsertionCost = rowIndex + 1;
+					var sourceChar = sourcePtr[rowIndex];
+
+					for (var columnIndex = 0; columnIndex < targetLength; columnIndex++)
+					{
+						var localCost = lastSubstitutionCost;
+						var deletionCost = previousRowPtr[columnIndex];
+						if (sourceChar != targetPtr[columnIndex])
+						{
+							localCost = Math.Min(lastInsertionCost, localCost);
+							localCost = Math.Min(deletionCost, localCost);
+							localCost++;
+						}
+						lastInsertionCost = localCost;
+						previousRowPtr[columnIndex] = localCost;
+						lastSubstitutionCost = deletionCost;
+					}
+				}
+			}
+
+			var result = previousRow[targetSpan.Length - 1];
+			ArrayPool<int>.Shared.Return(previousRow);
+			return result;
+		}
+	}
+}
