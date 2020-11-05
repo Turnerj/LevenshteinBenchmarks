7983516bfd00b6c75e9ecb07976fdd13ce6305a8
Turnerj <git@turnerj.com>
2020-11-05 16:09:51 +1030

    Added intrinsic calculation benchmarks

---
+++ 10_IntrinsicCalculationBenchmark.cs
@@ -0,0 +1,16 @@
+﻿using BenchmarkDotNet.Attributes;
+using LevenshteinBenchmarks.Implementations;
+
+namespace LevenshteinBenchmarks
+{
+	[Config(typeof(CodeSizeConfig))]
+	public class IntrinsicCalculationBenchmark : BaselineBenchmarks
+	{
+		[Benchmark(Baseline = true)]
+		public int Baseline() => LevenshteinBaseline.GetDistance(Source, Target);
+		[Benchmark]
+		public int PreviousBest() => LevenshteinIntrinsicTrimming.GetDistance(Source, Target);
+		[Benchmark]
+		public int IntrinsicCalculation() => LevenshteinIntrinsicCalculation.GetDistance(Source, Target);
+	}
+}
