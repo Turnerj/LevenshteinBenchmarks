ebb00ef03ab5b4be82ad4ef6aea8654c7b8eae97
Turnerj <git@turnerj.com>
2020-11-06 16:00:43 +1030

    Added final benchmark

---
+++ 12_FinalBenchmark.cs
@@ -0,0 +1,14 @@
+﻿using BenchmarkDotNet.Attributes;
+using LevenshteinBenchmarks.Implementations;
+
+namespace LevenshteinBenchmarks
+{
+	[Config(typeof(MemoryAndCodeSizeConfig))]
+	public class FinalBenchmark : BaselineBenchmarks
+	{
+		[Benchmark(Baseline = true)]
+		public int Baseline() => LevenshteinBaseline.GetDistance(Source, Target);
+		[Benchmark]
+		public int MultiThreaded() => LevenshteinMultiThreaded.GetDistance(Source, Target);
+	}
+}
