7877fa1a33ae2dd942bba2b5a4750ea6993b7249
Turnerj <git@turnerj.com>
2022-10-20 20:49:02 +1030

    Update .NET version

--- BenchmarkConfigs.cs
+++ BenchmarkConfigs.cs
@@ -1,62 +1,57 @@
-﻿using BenchmarkDotNet.Configs;
+﻿using BenchmarkDotNet.Columns;
+using BenchmarkDotNet.Configs;
 using BenchmarkDotNet.Diagnosers;
 using BenchmarkDotNet.Environments;
 using BenchmarkDotNet.Jobs;
 
-namespace LevenshteinBenchmarks
+namespace LevenshteinBenchmarks;
+
+public abstract class CommonConfig : ManualConfig
 {
-	public class StandardConfig : ManualConfig
+	protected CommonConfig()
 	{
-		public StandardConfig()
-		{
-			AddJob(Job.Default
-				.WithRuntime(CoreRuntime.Core50));
-		}
+		AddJob(Job.Default
+			.WithRuntime(CoreRuntime.Core60));
+
+		HideColumns(Column.RatioSD);
 	}
+}
 
-	public class MemoryConfig : ManualConfig
+public class StandardConfig : CommonConfig
+{
+	public StandardConfig() : base() { }
+}
+
+public class MemoryConfig : CommonConfig
+{
+	public MemoryConfig() : base()
 	{
-		public MemoryConfig()
-		{
-			AddJob(Job.Default
-				.WithRuntime(CoreRuntime.Core50));
-
-			AddDiagnoser(MemoryDiagnoser.Default);
-		}
+		AddDiagnoser(new MemoryDiagnoser(new MemoryDiagnoserConfig(false)));
 	}
+}
 
-	public class BranchPerfConfig : ManualConfig
+public class BranchPerfConfig : CommonConfig
+{
+	public BranchPerfConfig() : base()
 	{
-		public BranchPerfConfig()
-		{
-			AddJob(Job.Default
-				.WithRuntime(CoreRuntime.Core50));
-
-			AddHardwareCounters(HardwareCounter.BranchInstructions, HardwareCounter.BranchMispredictions);
-			AddDiagnoser(new DisassemblyDiagnoser(new DisassemblyDiagnoserConfig(maxDepth: 1, printSource: true)));
-		}
+		AddHardwareCounters(HardwareCounter.BranchInstructions, HardwareCounter.BranchMispredictions);
+		AddDiagnoser(new DisassemblyDiagnoser(new DisassemblyDiagnoserConfig(maxDepth: 1, printSource: true)));
 	}
+}
 
-	public class CodeSizeConfig : ManualConfig
+public class CodeSizeConfig : CommonConfig
+{
+	public CodeSizeConfig() : base()
 	{
-		public CodeSizeConfig()
-		{
-			AddJob(Job.Default
-				.WithRuntime(CoreRuntime.Core50));
-
-			AddDiagnoser(new DisassemblyDiagnoser(new DisassemblyDiagnoserConfig(maxDepth: 1, printSource: true)));
-		}
+		AddDiagnoser(new DisassemblyDiagnoser(new DisassemblyDiagnoserConfig(maxDepth: 1, printSource: true)));
 	}
+}
 
-	public class MemoryAndCodeSizeConfig : ManualConfig
+public class MemoryAndCodeSizeConfig : CommonConfig
+{
+	public MemoryAndCodeSizeConfig() : base()
 	{
-		public MemoryAndCodeSizeConfig()
-		{
-			AddJob(Job.Default
-				.WithRuntime(CoreRuntime.Core50));
-
-			AddDiagnoser(MemoryDiagnoser.Default);
-			AddDiagnoser(new DisassemblyDiagnoser(new DisassemblyDiagnoserConfig(maxDepth: 1, printSource: true)));
-		}
+		AddDiagnoser(new MemoryDiagnoser(new MemoryDiagnoserConfig(false)));
+		AddDiagnoser(new DisassemblyDiagnoser(new DisassemblyDiagnoserConfig(maxDepth: 1, printSource: true)));
 	}
 }
